66bbf8b5fd13816a2047a427af5d3c41f43e9200
fahathAMM <fahathammex90@gmail.com>
2025-07-05 18:15:16 +0400

    Update index.blade.php

--- resources/views/site/home/index.blade.php
+++ resources/views/site/home/index.blade.php
@@ -2,16 +2,18 @@
 @section('title', $title)
 
 @section('content')
-    <x-site.home.cat-banner />
+    {{-- <x-site.home.cat-banner /> --}}
 
     {{-- <x-site.home.testimonial /> --}}
 
     <x-site.home.popular-category :categories="$categories" />
 
-
-    <x-site.home.display-product :products="$products" />
+    {{-- <x-site.home.display-product :products="$products" /> --}}
 
     {{-- <x-site.home.explore /> --}}
+
+    <x-site.home.product :products="$products" />
+
 
     <x-site.home.iconbox />
 
